71177d326f8192dcc0663b6cf574d2a6079acdac
Mustafa <mustafa.said.acar@gmail.com>
2021-12-23 23:20:14 +0000

    Hello2

--- src/main/java/TestingGitHub1.java
+++ src/main/java/TestingGitHub1.java
@@ -2,6 +2,7 @@ public class TestingGitHub1 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
     }
 
 }
