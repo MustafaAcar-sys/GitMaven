d9c5e68b232a9a6c3ad87c83594ef7efecca26a1
Mustafa <mustafa.said.acar@gmail.com>
2021-12-23 23:12:24 +0000

    Learn3

--- src/test/java/TestingGithub.java
+++ src/test/java/TestingGithub.java
@@ -2,6 +2,7 @@ public class TestingGithub {
     public static void main(String[] args) {
         System.out.println("Learn1");
         System.out.println("Learn2");
+        System.out.println("Learn3");
 
     }
 }
