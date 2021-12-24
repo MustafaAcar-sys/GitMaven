5c2226fb9650e928890c08284a6f16428c99de2d
Mustafa <mustafa.said.acar@gmail.com>
2021-12-24 00:07:33 +0000

    Testing_Branch2 print out after lose

--- src/test/java/Testing_Branch2.java
+++ src/test/java/Testing_Branch2.java
@@ -2,6 +2,7 @@ public class Testing_Branch2 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello again");
+        System.out.println("Hello again after lose");
     }
 
 }
